82452e698ea535543d496d99368cdd8619ebd64f
unknown <983706550@qq.com>
2012-04-25 14:23:49 +0800

    add go to top

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -115,5 +115,21 @@ $(function () {
         _this.css({ "left": -left, "height": height }).animate({ left: -lastLeft + 20 }, "slow");
         _this.find("textarea").height(height - 12);
     }
-
+	
+	
+	(function() {
+    var $backToTopTxt = "返回顶部", $backToTopEle = $('<div id="backToTop"></div>').appendTo($("body"))
+        .text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
+            $("html, body").animate({ scrollTop: 0 }, 120);
+    }), $backToTopFun = function() {
+        var st = $(document).scrollTop(), winh = $(window).height();
+        (st > 0)? $backToTopEle.show(): $backToTopEle.hide();
+        //IE6下的定位
+        if (!window.XMLHttpRequest) {
+            $backToTopEle.css("top", st + winh - 166);
+        }
+    };
+    $(window).bind("scroll", $backToTopFun);
+    $(function() { $backToTopFun(); });
+	})();
 });
